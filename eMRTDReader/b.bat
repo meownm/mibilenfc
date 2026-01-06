0e42188d2fd42d602240d174e2929c51945ebd0a
meownm <101579754+meownm@users.noreply.github.com>
2026-01-06 15:37:42 +0300

    с

--- eMRTDReader/b.bat
+++ eMRTDReader/b.bat
@@ -1,9 +1,9 @@
 set ANDROID_HOME=C:\Android\sdk
 set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
 
-rd /s /q %USERPROFILE%\.gradle\caches
-
 gradlew --stop
+
+rd /s /q %USERPROFILE%\.gradle\caches
 
 gradlew clean
 gradlew assembleDebug
